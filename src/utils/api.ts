a3ef55537af8ddb5004b986378036cc02bdde9db
noturcat <chez.cus@gmail.com>
2025-07-28 04:10:02 +0800

    api route

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // dynamic based on env
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
